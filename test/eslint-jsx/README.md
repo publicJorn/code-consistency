4dfca89d3a8b6eb80501687c674b3416731f108e
Jorn Luiten <jorn@igne.nl>
2024-10-16 12:47:11 +0200

    add test projects for eslint

---
+++ test/eslint-jsx/README.md
@@ -0,0 +1,4 @@
+# Test for eslint config
+
+- Run `pnpm inspect-lint-config` to inspect the lint config
+- Run `pnpm lint` to see expected errors
---
+++ test/eslint-tsx/src/main.tsx
@@ -0,0 +1,24 @@
+// expect warning import order
+import { StrictMode, useState, useEffect } from 'react'
+import './main.css'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    console.log('count is', count) // expect warning console.log
+  }, []) // expect warning exhaustive-deps
+
+  return (
+    <div className="card">
+      <button onClick={() => setCount((count) => count + 1)}>Test {count}</button>
+    </div>
+  )
+}
