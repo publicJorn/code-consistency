e69ca143bb573e8fe32718fd7b9a151c53a1f871
Jorn Luiten <jorn@igne.nl>
2024-10-16 12:45:53 +0200

    eslint: implement flat config

--- packages/eslint-config/configs/react.js
+++ packages/eslint-config/configs/react.js
@@ -1,12 +1,29 @@
-module.exports = {
-  extends: [
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  plugins: ['react', 'react-hooks'],
-  rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react/prop-types': 'off',
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+
+export const configReact = [
+  {
+    name: 'devJorn/react',
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+    },
+
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+
+    settings: { react: { version: 'detect' } },
+
+    rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+
+      'react/jsx-no-target-blank': 'off',
+      'react/prop-types': 'off',
+    },
   },
-}
+]
